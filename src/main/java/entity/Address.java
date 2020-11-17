72ceb543c5d56e0a10d39c51735584ff0ada8db8
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2020-11-17 20:31:41 +0300

    Add type and address

---
+++ src/main/java/entity/Address.java
@@ -0,0 +1,83 @@
+package entity;
+
+public class Address {
+    private String country;
+    private String city;
+    private String street;
+    private Integer house;
+    private Integer flat;
+
+    public Address(String country, String city, String street, Integer house, Integer flat) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.flat = flat;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getHouse() {
+        return house;
+    }
+
+    public void setHouse(Integer house) {
+        this.house = house;
+    }
+
+    public Integer getFlat() {
+        return flat;
+    }
+
+    public void setFlat(Integer flat) {
+        this.flat = flat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Address address = (Address) o;
+
+        if (country != null ? !country.equals(address.country) : address.country != null) return false;
+        if (city != null ? !city.equals(address.city) : address.city != null) return false;
+        if (street != null ? !street.equals(address.street) : address.street != null) return false;
+        if (house != null ? !house.equals(address.house) : address.house != null) return false;
+        return flat != null ? flat.equals(address.flat) : address.flat == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = country != null ? country.hashCode() : 0;
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (street != null ? street.hashCode() : 0);
+        result = 31 * result + (house != null ? house.hashCode() : 0);
+        result = 31 * result + (flat != null ? flat.hashCode() : 0);
+        return result;
+    }
+
+
+}
--- src/main/java/entity/Contact.java
+++ src/main/java/entity/Contact.java
@@ -1,4 +1,150 @@
 package entity;
 
+import sun.util.resources.LocaleData;
+import type.FamilyStatusType;
+import type.SexType;
+
 public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocaleData birthday;
+    private SexType gender;
+    private String citizenship;
+    private FamilyStatusType familyStatus;
+    private String webSite;
+    private String email;
+    private String currentPlaceOfWork;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public LocaleData getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocaleData birthday) {
+        this.birthday = birthday;
+    }
+
+    public SexType getGender() {
+        return gender;
+    }
+
+    public void setGender(SexType gender) {
+        this.gender = gender;
+    }
+
+
+    public String getCitizenship() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+        this.citizenship = citizenship;
+    }
+
+    public FamilyStatusType getFamilyStatus() {
+        return familyStatus;
+    }
+
+    public void setFamilyStatus(FamilyStatusType familyStatus) {
+        this.familyStatus = familyStatus;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurrentPlaceOfWork() {
+        return currentPlaceOfWork;
+    }
+
+    public void setCurrentPlaceOfWork(String currentPlaceOfWork) {
+        this.currentPlaceOfWork = currentPlaceOfWork;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        if (firstName != null ? !firstName.equals(contact.firstName) : contact.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(contact.lastName) : contact.lastName != null) return false;
+        if (middleName != null ? !middleName.equals(contact.middleName) : contact.middleName != null) return false;
+        if (birthday != null ? !birthday.equals(contact.birthday) : contact.birthday != null) return false;
+        if (gender != contact.gender) return false;
+        if (citizenship != null ? !citizenship.equals(contact.citizenship) : contact.citizenship != null) return false;
+        if (familyStatus != contact.familyStatus) return false;
+        if (webSite != null ? !webSite.equals(contact.webSite) : contact.webSite != null) return false;
+        if (email != null ? !email.equals(contact.email) : contact.email != null) return false;
+        return currentPlaceOfWork != null ? currentPlaceOfWork.equals(contact.currentPlaceOfWork) : contact.currentPlaceOfWork == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (middleName != null ? middleName.hashCode() : 0);
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        result = 31 * result + (gender != null ? gender.hashCode() : 0);
+        result = 31 * result + (citizenship != null ? citizenship.hashCode() : 0);
+        result = 31 * result + (familyStatus != null ? familyStatus.hashCode() : 0);
+        result = 31 * result + (webSite != null ? webSite.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (currentPlaceOfWork != null ? currentPlaceOfWork.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", birthday=" + birthday +
+                ", gender=" + gender +
+                ", citizenship='" + citizenship + '\'' +
+                ", familyStatus=" + familyStatus +
+                ", webSite='" + webSite + '\'' +
+                ", email='" + email + '\'' +
+                ", currentPlaceOfWork='" + currentPlaceOfWork + '\'' +
+                '}';
+    }
 }
