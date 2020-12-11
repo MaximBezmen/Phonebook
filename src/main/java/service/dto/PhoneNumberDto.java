c20f038bc431187351a76a2232a31bd75bde04eb
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2020-12-11 19:06:18 +0300

    Add new entity

---
+++ src/main/java/service/dto/PhoneNumberDto.java
@@ -0,0 +1,11 @@
+package service.dto;
+
+import type.TypeNumber;
+
+public class PhoneNumberDto {
+    private Integer codeOfCountry;
+    private Integer operatorCode;
+    private String number;
+    private TypeNumber type;
+    private String comment;
+}
---
+++ src/main/java/type/TypeNumber.java
@@ -0,0 +1,10 @@
+package type;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public enum TypeNumber {
+    @JsonProperty("HOME")
+    HOME,
+    @JsonProperty("MOBILE")
+    MOBILE;
+}
