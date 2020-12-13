7c56614045b89cda68b51a3fa297f4b4ef8f2e93
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2020-12-13 15:54:16 +0300

    Add break

--- src/main/java/conrtroller/ContactCommand.java
+++ src/main/java/conrtroller/ContactCommand.java
@@ -10,12 +10,16 @@ public class ContactCommand implements Command {
         switch (methode) {
             case "GET":
                 new ContactController().doGet(request, response);
+                break;
             case "POST":
                 new ContactController().doPost(request, response);
+                break;
             case "PUT":
                 new ContactController().doPut(request, response);
+                break;
             case "DELETE":
                 new ContactController().doDelete(request, response);
+                break;
             default:
                 throw new IOException();
         }
