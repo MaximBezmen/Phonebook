1028fe9982a3ef46765b47885caa2e2d9acc178b
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-01-26 11:55:10 +0300

    Fix yml and test

---
+++ src/test/java/service/impl/ContactServiceImplTest.java
@@ -0,0 +1,15 @@
+package service.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactServiceImplTest {
+
+    @Test
+    void saveContact() {
+        int x = 3;
+        Assertions.assertEquals(2,x);
+    }
+}
